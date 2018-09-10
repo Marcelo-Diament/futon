ad85eeec3fca1b7899b149ee752050bd77dbfdfd
Marcelo-Diament <marcelodiament@gmail.com>
2018-09-10 16:14:02 -0300

    ongoing-testing

---
+++ wp-content/themes/futon/head.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html prefix="og: http://ogp.me/ns#">
+<html lang="pt">
+    <head>
+        <!-- RESETS -->
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+        <!-- <link rel="pingback" href="<?php bloginfo( //'pingback_url' ); ?>"> -->
+        <!-- META CACHE -->
+        <meta http-equiv="Cache-control" content="public">
+        <meta http-equiv="cache-control" content="max-age=0">
+        <meta name="revisit-after" content="7 days">
+        <meta http-equiv="expires" content="0">
+        <!-- MANIFESTO -->
+        <link rel="manifest" href="/manifest.json">
+        <!-- SCHEMA.ORG -->
+        <script type="application/ld+json">
+            {
+                "@context": "http://schema.org",
+                "@type": "Organization",
+                "url": "https://deezign.com.br",
+                "logo": "/favicons/favicon-512x512.png",
+                "description": "Móveis e Design Autoral",
+                "additionalType": "http://www.productontology.org/doc/Product_design",
+                "telephone" : "+55-11-3083-6212",
+                "email" : "contato@deezign.com.br",
+                "name" : "Deezign | concept place",
+                "alternateName" : "Deezign",
+                "address" : {
+                    "@type" : "PostalAddress",
+                    "addressCountry" : "BR",
+                    "addressLocality" : "São Paulo",
+                    "addressRegion" : "São Paulo",
+                    "postalCode" : "05089-001",
+                    "streetAddress" : "Rua Guaipá, Vila Leopoldina"
+                },
+                "brand": {
+                    "@type": "Thing",
+                    "name": "Deezign"
+                },
+                "sameAs" : [ "https://www.instagram.com/deezign.br/", "https://br.pinterest.com/deezignbr/", "https://www.facebook.com/Deezign-263005504442130/", "https://twitter.com/deezignbr"],
+                "contactPoint": [{
+                    "@type": "ContactPoint",
+                    "telephone": "+55-11-3083-6212",
+                    "contactType": "sales",
+                    "areaServed": "BR",
+                    "availableLanguage": ["Portuguese", "French"]
+                }]
+            }
+        </script>
+        <!-- ESTILO -->
+        <link async href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
+        <link async href="https://fonts.googleapis.com/css?family=Poiret+One|Raleway|Quicksand|Nunito" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="style.css">
+        <!-- META DATA -->
+        <meta name="identifier-URL" content="https://www.deezign.com.br">
+        <meta name="url" content="https://www.deezign.com.br">
+        <meta name="abstract" content="Design Autoral">
+        <meta name="author" content="Futon Company, Deezign, Marcelo Diament, Gabriel Montibeller">
+        <meta name="robots" content="index,follow">
+        <meta name="contact" content="contato@deezign.com.br">
+        <meta name="reply-to" content="contato@deezign.com.br">
+        <meta name="copyright" content="Deezign">
+        <meta name="rating" content="general">
+        <meta name="web_author" content="Futon Company, Deezign, Marcelo Diament, Gabriel Montibeller"> 
+        <!-- META OG -->
+        <meta property="og:site_name" content="Deezign">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Deezign">
+        <meta property="og:description" content="Móveis e Design Autoral">
+        <meta property="og:url" content="https://deezign.com.br">
+        <meta property="og:locale" content="pt-BR">
+        <meta name="og:locality" content="São Paulo">
+        <meta name="og:region" content="SP">
+        <meta name="og:country-name" content="BR">
+        <meta property="og:type" content="website">
+        <meta property="og:image" content="/favicons/deezign-thumb-1200x630.png">
+        <meta property="og:image:alt" content="Deezign | Móveis e Design Autoral">
+        <meta property="og:image:url" content="/favicons/deezign-thumb-1200x630.png">
+        <meta property="og:image:secure_url" content="/favicons/deezign-thumb-1200x630.png">
+        <meta property="og:image:type" content="img/png">
+        <meta property="og:image:width" content="1200">
+        <meta property="og:image:height" content="630">
+        <!-- MOBILE SPECS -->
+        <meta name="application-name" content="Deezign">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="Deezign"> 
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <meta name="msapplication-TileColor" content="#FFFFFF">
+        <meta name="theme-color" content="#FFFFFF">
+        <!-- FAVICONS -->
+        <!--[if IE]><link rel="shortcut icon" href="favicons/favicon.ico"><![endif]-->
+        <meta name="msapplication-TileImage" content="favicons/tile.png">
+        <link rel="icon" type="image/png" sizes="512x512" href="favicons/favicon-512x512.png">
+        <link rel="icon" type="image/png" sizes="228x228" href="favicons/favicon-228x228.png">
+        <link rel="shortcut icon" sizes="196x196" href="favicons/favicon-196.png">
+        <link rel="icon" sizes="192x192" href="favicons/touch-icon-192x192.png">
+        <link rel="apple-touch-icon-precomposed" sizes="180x180" href="favicons/apple-touch-icon-precomposed-180x180.png">
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="favicons/apple-touch-icon-precomposed-152x152.png">
+        <link rel="icon" type="image/png" sizes="128x128" href="favicons/favicon-128x128.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="favicons/apple-touch-icon-precomposed-144x144.png">
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="favicons/apple-touch-icon-precomposed-120x120.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="favicons/apple-touch-icon-precomposed-114x114.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png">
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="favicons/apple-touch-icon-precomposed-76x76.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="favicons/apple-touch-icon-precomposed-72x72.png">
+        <link rel="apple-touch-icon-precomposed" sizes="60x60"href="favicons/apple-touch-icon-precomposed-60x60.png">
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="favicons/apple-touch-icon-precomposed-57x57.png">
+        <link rel="icon" type="image/png" sizes="48x48" href="favicons/android-icon-48x48.png">
+        <link rel="icon" type="image/png" sizes="36x36" href="favicons/android-icon-36x36.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
